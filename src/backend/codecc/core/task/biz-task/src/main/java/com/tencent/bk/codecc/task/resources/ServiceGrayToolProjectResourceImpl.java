800f289d124364ec7c3dbec0bb8620a4f40df0f3
ruitaoyuan <ruitaoyuan@tencent.com>
2023-03-05 02:11:01 +0800

    --story=879734483 【CodeCC】内部版代码同步到社区版
    (cherry picked from commit 1afcd131e8801a39e36131233e26bc1c0299ac19)

--- src/backend/codecc/core/task/biz-task/src/main/java/com/tencent/bk/codecc/task/resources/ServiceGrayToolProjectResourceImpl.java
+++ src/backend/codecc/core/task/biz-task/src/main/java/com/tencent/bk/codecc/task/resources/ServiceGrayToolProjectResourceImpl.java
@@ -1,25 +1,29 @@
 package com.tencent.bk.codecc.task.resources;
 
 import com.tencent.bk.codecc.task.api.ServiceGrayToolProjectResource;
+import com.tencent.bk.codecc.task.service.GrayToolProjectService;
 import com.tencent.bk.codecc.task.vo.GrayTaskStatVO;
 import com.tencent.bk.codecc.task.vo.GrayToolProjectVO;
 import com.tencent.devops.common.api.pojo.codecc.Result;
 import com.tencent.devops.common.web.RestResource;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestResource
 public class ServiceGrayToolProjectResourceImpl implements ServiceGrayToolProjectResource {
 
+    @Autowired
+    private GrayToolProjectService grayToolProjectService;
+
     @Override
     public Result<GrayToolProjectVO> getGrayToolProjectInfoByProjectId(String projectId) {
-        return new Result<>(null);
+        return new Result<>(grayToolProjectService.findGrayInfoByProjectId(projectId));
     }
 
     @Override
     public Result<List<GrayToolProjectVO>> getGrayToolProjectByProjectIds(Set<String> projectIdSet) {
-        return new Result<>(Collections.emptyList());
+        return new Result<>(grayToolProjectService.findGrayToolProjectByProjectIds(projectIdSet));
     }
 
     @Override
