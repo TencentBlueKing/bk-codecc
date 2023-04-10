2c99230f3fc8f48ab85a9f3cbfb3a0b70af66990
ruitaoyuan <ruitaoyuan@tencent.com>
2023-04-10 15:40:38 +0800

    --story=881895981 CodeCC内部功能同步到社区版
    (cherry picked from commit ae0cdc8f535a2b1b57e3dcbb0f98c6afa05094bb)

--- src/backend/codecc/core/task/biz-task/src/main/java/com/tencent/bk/codecc/task/service/impl/MetaServiceImpl.java
+++ src/backend/codecc/core/task/biz-task/src/main/java/com/tencent/bk/codecc/task/service/impl/MetaServiceImpl.java
@@ -26,11 +26,6 @@
 
 package com.tencent.bk.codecc.task.service.impl;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.tencent.bk.codecc.defect.api.ServiceCheckerSetRestResource;
 import com.tencent.bk.codecc.task.constant.TaskConstants;
 import com.tencent.bk.codecc.task.dao.CommonDao;
 import com.tencent.bk.codecc.task.dao.mongorepository.BaseDataRepository;
@@ -38,7 +33,6 @@ import com.tencent.bk.codecc.task.dao.mongorepository.TaskRepository;
 import com.tencent.bk.codecc.task.dao.mongorepository.ToolMetaRepository;
 import com.tencent.bk.codecc.task.model.BaseDataEntity;
 import com.tencent.bk.codecc.task.model.OpenSourceCheckerSet;
-import com.tencent.bk.codecc.task.model.SpecialCheckerSetConfig;
 import com.tencent.bk.codecc.task.model.TaskInfoEntity;
 import com.tencent.bk.codecc.task.model.ToolMetaEntity;
 import com.tencent.bk.codecc.task.service.MetaService;
@@ -48,14 +42,12 @@ import com.tencent.bk.codecc.task.vo.OpenScanAndPreProdCheckerSetMapVO;
 import com.tencent.bk.codecc.task.vo.checkerset.OpenSourceCheckerSetVO;
 import com.tencent.devops.common.api.ToolMetaBaseVO;
 import com.tencent.devops.common.api.ToolMetaDetailVO;
-import com.tencent.devops.common.api.checkerset.CheckerSetVO;
 import com.tencent.devops.common.auth.api.external.AuthExPermissionApi;
 import com.tencent.devops.common.client.Client;
 import com.tencent.devops.common.constant.ComConstants;
 import com.tencent.devops.common.constant.RedisKeyConstants;
 import com.tencent.devops.common.service.ToolMetaCacheService;
 import com.tencent.devops.common.util.CompressionUtils;
-import com.tencent.devops.common.codecc.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
@@ -74,11 +66,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static com.tencent.devops.common.api.auth.HeaderKt.AUTH_HEADER_DEVOPS_TASK_ID;
@@ -116,25 +105,6 @@ public class MetaServiceImpl implements MetaService {
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
-
-    private final LoadingCache<String, OpenScanAndEpcToolNameMapVO> openScanAndEpcToolCache = CacheBuilder.newBuilder()
-            .refreshAfterWrite(10, TimeUnit.MINUTES)
-            .build(new CacheLoader<String, OpenScanAndEpcToolNameMapVO>() {
-                @Override
-                public OpenScanAndEpcToolNameMapVO load(String mockId) {
-                    return doGetOpenScanAndEpcToolNameMap();
-                }
-            });
-
-    private final LoadingCache<String, OpenScanAndPreProdCheckerSetMapVO> openScanAndPreProdCheckerSetCache =
-            CacheBuilder.newBuilder()
-                    .refreshAfterWrite(30, TimeUnit.SECONDS)
-                    .build(new CacheLoader<String, OpenScanAndPreProdCheckerSetMapVO>() {
-                        @Override
-                        public OpenScanAndPreProdCheckerSetMapVO load(String mockId) {
-                            return doGetOpenScanAndPreProdCheckerSetMap();
-                        }
-                    });
 
     @Override
     public List<ToolMetaBaseVO> toolList(Boolean isDetail) {
@@ -355,243 +325,14 @@ public class MetaServiceImpl implements MetaService {
 
     @Override
     public OpenScanAndEpcToolNameMapVO getOpenScanAndEpcToolNameMap() {
-        return openScanAndEpcToolCache.getUnchecked("");
+        return new OpenScanAndEpcToolNameMapVO();
     }
 
     @Override
     public OpenScanAndPreProdCheckerSetMapVO getOpenScanAndPreProdCheckerSetMap() {
-        return openScanAndPreProdCheckerSetCache.getUnchecked("");
+        return new OpenScanAndPreProdCheckerSetMapVO();
     }
 
-    private OpenScanAndEpcToolNameMapVO doGetOpenScanAndEpcToolNameMap() {
-        //参考：PipelineTaskRegisterServiceImpl#setOpenScanCheckerSetsAccordingToLanguage
-        //PipelineTaskRegisterServiceImpl#setEpcScanCheckerSetsAccordingToLanguage
-
-        log.info("start to do get open scan and ecp tool name map");
-
-        List<BaseDataEntity> baseDataList = baseDataRepository.findAllByParamType(ComConstants.KEY_CODE_LANG);
-        if (CollectionUtils.isEmpty(baseDataList)) {
-            return null;
-        }
-
-        HashMap<String, Set<String>> openScanToolNameMap = new HashMap<>();
-        HashMap<String, Set<String>> epcToolNameMap = new HashMap<>();
-
-        // 遍历所有语言元数据
-        baseDataList.forEach(data -> {
-            // 获取开源扫描规则集工具
-            openScanToolNameMap.put(data.getLangFullKey(), new HashSet<>());
-            if (!CollectionUtils.isEmpty(data.getOpenSourceCheckerSets())) {
-                Set<CheckerSetVO> checkerSetVOS = data.getOpenSourceCheckerSets().stream()
-                        .map(it -> {
-                            CheckerSetVO checkerSetVO = new CheckerSetVO();
-                            checkerSetVO.setCheckerSetId(it.getCheckerSetId());
-                            checkerSetVO.setVersion(it.getVersion());
-                            return checkerSetVO;
-                        }).collect(Collectors.toSet());
-
-                // 获取规则集对应的工具，并填充到map
-                List<CheckerSetVO> checkerSetList = client.get(ServiceCheckerSetRestResource.class)
-                        .queryCheckerSetsForOpenScan(checkerSetVOS).getData();
-                if (checkerSetList != null) {
-                    checkerSetList.forEach(it -> {
-                        openScanToolNameMap.get(data.getLangFullKey()).addAll(it.getToolList());
-                    });
-                }
-            }
-
-            // 获取epc规范规则集工具
-            epcToolNameMap.put(data.getLangFullKey(), new HashSet<>());
-            if (!CollectionUtils.isEmpty(data.getEpcCheckerSets())) {
-                Set<CheckerSetVO> checkerSetVOS = data.getEpcCheckerSets().stream()
-                        .map(it -> {
-                            CheckerSetVO checkerSetVO = new CheckerSetVO();
-                            checkerSetVO.setCheckerSetId(it.getCheckerSetId());
-                            checkerSetVO.setVersion(it.getVersion());
-                            return checkerSetVO;
-                        }).collect(Collectors.toSet());
-
-                // 获取规则集对应的工具，并填充到map
-                List<CheckerSetVO> checkerSetList = client.get(ServiceCheckerSetRestResource.class)
-                        .queryCheckerSetsForOpenScan(checkerSetVOS).getData();
-                if (checkerSetList != null) {
-                    checkerSetList.forEach(it -> {
-                        epcToolNameMap.get(data.getLangFullKey()).addAll(it.getToolList());
-                    });
-                }
-            }
-        });
-
-        log.info("finish to do get open scan and ecp tool name map: {}, {}", openScanToolNameMap, epcToolNameMap);
-
-        return new OpenScanAndEpcToolNameMapVO(openScanToolNameMap, epcToolNameMap);
-    }
-
-    private OpenScanAndPreProdCheckerSetMapVO doGetOpenScanAndPreProdCheckerSetMap() {
-
-        log.info("start to get open and pre prod checker set map");
-
-        List<BaseDataEntity> baseDataList = baseDataRepository.findAllByParamType(ComConstants.KEY_CODE_LANG);
-
-        Map<String, List<OpenSourceCheckerSetVO>> openScanCheckerSetMap = new HashMap<>(8);
-        Map<String, List<OpenSourceCheckerSetVO>> preProdOpenScanCheckerSetMap = new HashMap<>(8);
-        Map<String, List<OpenSourceCheckerSetVO>> communityOpenScanCheckerSetMap = new HashMap<>(8);
-        Map<String, List<OpenSourceCheckerSetVO>> preProdCommunityOpenScanCheckerSetMap = new HashMap<>(8);
-        Map<String, List<OpenSourceCheckerSetVO>> epcScanCheckerSetMap = new HashMap<>(8);
-        Map<String, List<OpenSourceCheckerSetVO>> preProdEpcScanCheckerSetMap = new HashMap<>(8);
-
-        for (BaseDataEntity baseDataEntity : baseDataList) {
-            openScanCheckerSetMap.put(baseDataEntity.getLangFullKey(), new ArrayList<>());
-            preProdOpenScanCheckerSetMap.put(baseDataEntity.getLangFullKey(), new ArrayList<>());
-            communityOpenScanCheckerSetMap.put(baseDataEntity.getLangFullKey(), new ArrayList<>());
-            preProdCommunityOpenScanCheckerSetMap.put(baseDataEntity.getLangFullKey(), new ArrayList<>());
-            epcScanCheckerSetMap.put(baseDataEntity.getLangFullKey(), new ArrayList<>());
-            preProdEpcScanCheckerSetMap.put(baseDataEntity.getLangFullKey(), new ArrayList<>());
-
-            SpecialCheckerSetConfig config = new SpecialCheckerSetConfig();
-            if (!StringUtils.isBlank(baseDataEntity.getParamExtend6())) {
-                config = JsonUtil.INSTANCE.to(
-                        baseDataEntity.getParamExtend6(), new TypeReference<SpecialCheckerSetConfig>() {
-                        });
-            }
-
-            List<OpenSourceCheckerSetVO> openCheckerSetVOList = getProdCheckerSets(config, baseDataEntity,
-                    ComConstants.PROD_TENCENT_OPENSOURCE_CHECKER_SET_TIME_GAP);
-            openScanCheckerSetMap.get(baseDataEntity.getLangFullKey()).addAll(openCheckerSetVOList);
-
-            List<OpenSourceCheckerSetVO> epcCheckerSetVOList = getProdCheckerSets(config, baseDataEntity,
-                    ComConstants.PROD_TENCENT_EPC_OPENSOURCE_CHECKER_SET_TIME_GAP);
-            epcScanCheckerSetMap.get(baseDataEntity.getLangFullKey()).addAll(epcCheckerSetVOList);
-
-            if (!CollectionUtils.isEmpty(config.getProdCommunityOpenScan())) {
-                List<OpenSourceCheckerSetVO> checkerSetVOList = config.getProdCommunityOpenScan().stream().map(it -> {
-                    OpenSourceCheckerSetVO checkerSetVO = new OpenSourceCheckerSetVO();
-                    BeanUtils.copyProperties(it, checkerSetVO);
-                    return checkerSetVO;
-                }).collect(Collectors.toList());
-                communityOpenScanCheckerSetMap.get(baseDataEntity.getLangFullKey()).addAll(checkerSetVOList);
-            }
-
-            //preProd
-            List<OpenSourceCheckerSetVO> preProdOpenCheckerSetVOList = getPreProdCheckerSets(
-                    config, baseDataEntity, ComConstants.PRE_PROD_TENCENT_OPENSOURCE_CHECKER_SET_TIME_GAP);
-            preProdOpenScanCheckerSetMap.get(baseDataEntity.getLangFullKey()).addAll(preProdOpenCheckerSetVOList);
-
-            List<OpenSourceCheckerSetVO> preProdEpcCheckerSetVOList = getPreProdCheckerSets(
-                    config, baseDataEntity, ComConstants.PRE_PROD_TENCENT_EPC_OPENSOURCE_CHECKER_SET_TIME_GAP);
-            preProdEpcScanCheckerSetMap.get(baseDataEntity.getLangFullKey()).addAll(preProdEpcCheckerSetVOList);
-
-            if (!CollectionUtils.isEmpty(config.getPreProdCommunityOpenScan())) {
-                List<OpenSourceCheckerSetVO> checkerSetVOList = config.getPreProdCommunityOpenScan()
-                        .stream().map(it -> {
-                            OpenSourceCheckerSetVO checkerSetVO = new OpenSourceCheckerSetVO();
-                            BeanUtils.copyProperties(it, checkerSetVO);
-                            return checkerSetVO;
-                        }).collect(Collectors.toList());
-                preProdCommunityOpenScanCheckerSetMap.get(baseDataEntity.getLangFullKey()).addAll(checkerSetVOList);
-            }
-        }
-
-        log.info("start to get time gap");
-        BaseDataEntity preProdBaseData =
-                baseDataRepository.findFirstByParamType(ComConstants.PRE_PROD_TENCENT_OPENSOURCE_CHECKER_SET_TIME_GAP);
-        BaseDataEntity prodBaseData =
-                baseDataRepository.findFirstByParamType(ComConstants.PROD_TENCENT_OPENSOURCE_CHECKER_SET_TIME_GAP);
-
-        log.info("finish to get prod and pre prod. prod start time is {}, end time is {}, "
-                        + "preProd start time is {}, end time is {}", prodBaseData.getParamExtend1(),
-                prodBaseData.getParamExtend2(), preProdBaseData.getParamExtend1(), preProdBaseData.getParamExtend2());
-        OpenScanAndPreProdCheckerSetMapVO result = new OpenScanAndPreProdCheckerSetMapVO();
-        result.setProdOpenScanTimeGap(new OpenScanAndPreProdCheckerSetMapVO.TimeUnit(
-                prodBaseData.getParamExtend1(),
-                prodBaseData.getParamExtend2()));
-        result.setPreProdOpenScanTimeGap(new OpenScanAndPreProdCheckerSetMapVO.TimeUnit(
-                preProdBaseData.getParamExtend1(),
-                preProdBaseData.getParamExtend2()));
-
-        BaseDataEntity prodCommunityBaseData = baseDataRepository
-                .findFirstByParamType(ComConstants.PROD_TENCENT_COMMUNITY_OPENSOURCE_CHECKER_SET_TIME_GAP);
-        result.setProdCommunityOpenScanTimeGap(new OpenScanAndPreProdCheckerSetMapVO.TimeUnit(
-                prodCommunityBaseData.getParamExtend1(),
-                prodCommunityBaseData.getParamExtend2()));
-        BaseDataEntity preProdCommunityBaseData = baseDataRepository
-                .findFirstByParamType(ComConstants.PRE_PROD_TENCENT_COMMUNITY_OPENSOURCE_CHECKER_SET_TIME_GAP);
-        result.setPreProdCommunityOpenScanTimeGap(new OpenScanAndPreProdCheckerSetMapVO.TimeUnit(
-                preProdCommunityBaseData.getParamExtend1(),
-                preProdCommunityBaseData.getParamExtend2()));
-
-        BaseDataEntity prodEpcBaseData = baseDataRepository
-                .findFirstByParamType(ComConstants.PROD_TENCENT_EPC_OPENSOURCE_CHECKER_SET_TIME_GAP);
-        result.setProdEpcScanTimeGap(new OpenScanAndPreProdCheckerSetMapVO.TimeUnit(
-                prodEpcBaseData.getParamExtend1(),
-                prodEpcBaseData.getParamExtend2()));
-        BaseDataEntity preProdEpcBaseData = baseDataRepository
-                .findFirstByParamType(ComConstants.PRE_PROD_TENCENT_EPC_OPENSOURCE_CHECKER_SET_TIME_GAP);
-        result.setPreProdEpcScanTimeGap(new OpenScanAndPreProdCheckerSetMapVO.TimeUnit(
-                preProdEpcBaseData.getParamExtend1(),
-                preProdEpcBaseData.getParamExtend2()));
-
-        result.setProdOpenScan(openScanCheckerSetMap);
-        result.setProdCommunityOpenScan(communityOpenScanCheckerSetMap);
-        result.setProdEpcScan(epcScanCheckerSetMap);
-
-        result.setPreProdOpenScan(preProdOpenScanCheckerSetMap);
-        result.setPreProdCommunityOpenScan(preProdCommunityOpenScanCheckerSetMap);
-        result.setPreProdEpcScan(preProdEpcScanCheckerSetMap);
-        return result;
-    }
-
-    private List<OpenSourceCheckerSetVO> getProdCheckerSets(SpecialCheckerSetConfig config,
-                                                            BaseDataEntity baseDataEntity, String type) {
-        List<OpenSourceCheckerSetVO> prodCheckerSetVOList;
-        if (type.equals(ComConstants.PROD_TENCENT_OPENSOURCE_CHECKER_SET_TIME_GAP)) {
-            if (CollectionUtils.isEmpty(config.getOpenSourceCheckerSets())) {
-                log.info("{} open prod config is empty.", baseDataEntity.getLangFullKey());
-                prodCheckerSetVOList = transferCheckerSetToCheckerSetVO(baseDataEntity.getOpenSourceCheckerSets());
-            } else {
-                log.info("{} open prod config is not empty: {}", baseDataEntity.getLangFullKey(),
-                        config.getOpenSourceCheckerSets());
-                prodCheckerSetVOList = transferCheckerSetToCheckerSetVO(config.getOpenSourceCheckerSets());
-            }
-        } else {
-            if (CollectionUtils.isEmpty(config.getEpcCheckerSets())) {
-                log.info("{} epc prod config is empty.", baseDataEntity.getLangFullKey());
-                prodCheckerSetVOList = transferCheckerSetToCheckerSetVO(baseDataEntity.getEpcCheckerSets());
-            } else {
-                log.info("{} epc prod config is not empty: {}", baseDataEntity.getLangFullKey(),
-                        config.getEpcCheckerSets());
-                prodCheckerSetVOList = transferCheckerSetToCheckerSetVO(config.getEpcCheckerSets());
-            }
-        }
-        return prodCheckerSetVOList;
-    }
-
-    private List<OpenSourceCheckerSetVO> getPreProdCheckerSets(SpecialCheckerSetConfig config,
-                                                               BaseDataEntity baseDataEntity, String type) {
-        List<OpenSourceCheckerSetVO> preProdCheckerSetVOList;
-        if (type.equals(ComConstants.PRE_PROD_TENCENT_OPENSOURCE_CHECKER_SET_TIME_GAP)) {
-            if (CollectionUtils.isEmpty(config.getPreProdOpenSourceCheckerSets())) {
-                log.info("{} open preProd config is empty.", baseDataEntity.getLangFullKey());
-                preProdCheckerSetVOList = transferCheckerSetToCheckerSetVO(
-                        baseDataEntity.getOpenSourceCheckerSets());
-            } else {
-                log.info("{} open preProd config is not empty: {}", baseDataEntity.getLangFullKey(),
-                        config.getPreProdOpenSourceCheckerSets());
-                preProdCheckerSetVOList = transferCheckerSetToCheckerSetVO(
-                        config.getPreProdOpenSourceCheckerSets());
-            }
-        } else {
-            if (CollectionUtils.isEmpty(config.getPreProdEpcCheckerSets())) {
-                log.info("{} epc prePreProd config is empty.", baseDataEntity.getLangFullKey());
-                preProdCheckerSetVOList = transferCheckerSetToCheckerSetVO(baseDataEntity.getEpcCheckerSets());
-            } else {
-                log.info("{} epc preProd config is not empty: {}", baseDataEntity.getLangFullKey(),
-                        config.getPreProdEpcCheckerSets());
-                preProdCheckerSetVOList = transferCheckerSetToCheckerSetVO(config.getPreProdEpcCheckerSets());
-            }
-        }
-        return preProdCheckerSetVOList;
-    }
 
     public List<OpenSourceCheckerSetVO> transferCheckerSetToCheckerSetVO(List<OpenSourceCheckerSet> checkerSets) {
         List<OpenSourceCheckerSetVO> checkerSetVOList = checkerSets.stream().filter(it ->
