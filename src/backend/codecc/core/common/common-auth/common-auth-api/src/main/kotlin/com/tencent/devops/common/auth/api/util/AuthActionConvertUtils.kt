3e8cb4a872a07bfdad32d924e26cdf541cb36dee
ruitaoyuan <ruitaoyuan@tencent.com>
2023-06-01 11:39:50 +0800

    codecc 版本更新

--- src/backend/codecc/core/common/common-auth/common-auth-api/src/main/kotlin/com/tencent/devops/common/auth/api/util/AuthActionConvertUtils.kt
+++ src/backend/codecc/core/common/common-auth/common-auth-api/src/main/kotlin/com/tencent/devops/common/auth/api/util/AuthActionConvertUtils.kt
@@ -3,47 +3,42 @@ package com.tencent.devops.common.auth.api.util
 import com.tencent.devops.common.auth.api.pojo.external.CodeCCAuthAction
 import com.tencent.devops.common.auth.api.pojo.external.PipelineAuthAction
 
-class AuthActionConvertUtils {
+object AuthActionConvertUtils {
 
 
-    companion object {
-
-        fun covert(actions: List<CodeCCAuthAction>): List<PipelineAuthAction> {
-            if (actions.isEmpty()) {
-                return emptyList()
-            }
-            val pipelineActions = mutableSetOf<PipelineAuthAction>()
-            actions.forEach {
-                pipelineActions.addAll(covert(it))
-            }
-            return pipelineActions.toList()
+    fun covert(actions: List<CodeCCAuthAction>): List<PipelineAuthAction> {
+        if (actions.isEmpty()) {
+            return emptyList()
         }
-
-        private fun covert(action: CodeCCAuthAction?): List<PipelineAuthAction> {
-            if (action == null) {
-                return emptyList()
-            }
-            return when (action) {
-                CodeCCAuthAction.TASK_MANAGE ->
-                    listOf<PipelineAuthAction>(
-                        PipelineAuthAction.EDIT
-                    )
-                CodeCCAuthAction.ANALYZE ->
-                    listOf<PipelineAuthAction>(
-                        PipelineAuthAction.EXECUTE
-                    )
-                CodeCCAuthAction.DEFECT_MANAGE ->
-                    listOf<PipelineAuthAction>(
-                        PipelineAuthAction.EXECUTE
-                    )
-                else ->
-                    listOf<PipelineAuthAction>(
-                        PipelineAuthAction.VIEW
-                    )
-            }
+        val pipelineActions = mutableSetOf<PipelineAuthAction>()
+        actions.forEach {
+            pipelineActions.addAll(covert(it))
         }
-
+        return pipelineActions.toList()
     }
 
+    private fun covert(action: CodeCCAuthAction?): List<PipelineAuthAction> {
+        if (action == null) {
+            return emptyList()
+        }
+        return when (action) {
+            CodeCCAuthAction.TASK_MANAGE ->
+                listOf<PipelineAuthAction>(
+                    PipelineAuthAction.EDIT
+                )
+            CodeCCAuthAction.ANALYZE ->
+                listOf<PipelineAuthAction>(
+                    PipelineAuthAction.EXECUTE
+                )
+            CodeCCAuthAction.DEFECT_MANAGE ->
+                listOf<PipelineAuthAction>(
+                    PipelineAuthAction.EXECUTE
+                )
+            else ->
+                listOf<PipelineAuthAction>(
+                    PipelineAuthAction.VIEW
+                )
+        }
+    }
 
 }
